18d83bf8e0a18f9c9c714d886e78fa20c485611c
Manish Y <manish@gmail.com>
2025-08-11 11:34:59 +0530

    add services end point

--- src/main/java/com/SpringBoot/ManishIt/controllers/HomeController.java
+++ src/main/java/com/SpringBoot/ManishIt/controllers/HomeController.java
@@ -10,5 +10,16 @@ public class HomeController {
 	public String home() {
 		return "welcome to my home page...";
 	}
+	
+	@GetMapping(value = "/contect")
+	public String getContaict() {
+		return "you can contect on Manish@gmail.com";
+	}
+	
+	@GetMapping(value = "/services")
+	public String getServices() {
+		return "you can contect on Manish@gmail.com";
+	}
+
 
 }
